5f886fc2ae8889a807036332c9a813796a942697
Eve <ptitediablessegothique@hotmail.fr>
2019-04-30 10:09:54 +0200

    control + cam ok

--- Assets/CameraBehaviour.cs
+++ Assets/CameraBehaviour.cs
@@ -18,19 +18,9 @@ public class CameraBehaviour : MonoBehaviour
         float rangeBewteenPlayers = Vector3.Distance(Player1.position, Player2.position);
         Debug.Log(rangeBewteenPlayers);
 
-        transform.position = Player1.position + Player2.position / 2;
-
+        transform.position = (Player1.position + Player2.position) / 2;
         transform.position = new Vector3(transform.position.x- 0.3f, transform.position.y+26, transform.position.z-58);
-
-
-        //if (rangeBewteenPlayers > 20 && camComponent.orthographicSize < 10)
-        //{
-        //    camComponent.orthographicSize = rangeBewteenPlayers / 0.50f;
-        //}
-        //else if (rangeBewteenPlayers < 20 && camComponent.orthographicSize > 6) {
-        //    camComponent.orthographicSize = rangeBewteenPlayers / 1.50f;
-        //}
-
+        
         float camSize = rangeBewteenPlayers / 2;
 
         camComponent.orthographicSize = Mathf.Clamp(camSize, 6, 10);
