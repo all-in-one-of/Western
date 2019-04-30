3100a913caf952068e01b87d8ddb8632866644d4
Théophile Candelier <thophilecandelier@yahoo.fr>
2019-04-30 10:33:05 +0200

    Added shaderforge + PlayerSpawn & EnemySpawn

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -7,8 +7,13 @@ public class PlayerManager : Singleton<PlayerManager>
     [System.NonSerialized] public List<PlayerBehaviour> players;
     public GameObject playerPrefab;
 
-    public void SpawnPlayer(Transform spawnPoint)
+    public void SpawnPlayer(int playerNumber)
     {
-        players.Add(Instantiate(playerPrefab, spawnPoint.transform.position, Quaternion.identity).GetComponent<PlayerBehaviour>());
+        if (playerNumber > PlayerSpawn.spawns.Count)
+        {
+            Debug.Log("no spawn point available");
+            return;
+        }
+        players.Add(Instantiate(playerPrefab, PlayerSpawn.spawns[playerNumber].self.position, Quaternion.identity).GetComponent<PlayerBehaviour>());
     }
 }
