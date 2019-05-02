54564a8823d85ca84728719cd00a8017dd0721b7
Théophile Candelier <thophilecandelier@yahoo.fr>
2019-05-02 10:14:45 +0200

    Menu prefab + player stats duplicated

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
-    
+    public GameObject canvasPrefab;
+    public Menu menu;
+
+    public void SpawnCanvas()
+    {
+        menu=Instantiate(canvasPrefab).GetComponent<Menu>();
+    }
 }
